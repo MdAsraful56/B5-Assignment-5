c303deabc290c2a046aa2aa9f9b249b623f2703e
MdAsraful56 <work.mdasraful56@gmail.com>
2025-08-01 13:52:14 +0600

    some feature editing. ex: config, errro, helpers, interface, middlewares, rotes, utils

---
+++ src/app/routes/index.ts
@@ -0,0 +1,30 @@
+import { Router } from 'express';
+import { AuthRoutes } from '../modules/auth/auth.route';
+import { DriverRoutes } from '../modules/driver/driver.route';
+import { RideRoutes } from '../modules/ride/ride.route';
+import { UserRoutes } from '../modules/user/user.route';
+
+export const router = Router();
+
+const moduleRoutes = [
+    {
+        path: '/user',
+        route: UserRoutes,
+    },
+    {
+        path: '/auth',
+        route: AuthRoutes,
+    },
+    {
+        path: '/ride',
+        route: RideRoutes,
+    },
+    {
+        path: '/driver',
+        route: DriverRoutes,
+    },
+];
+
+moduleRoutes.forEach((route) => {
+    router.use(route.path, route.route);
+});
---
+++ src/app/utils/sendAdmin.ts
@@ -0,0 +1,41 @@
+import bcryptjs from 'bcryptjs';
+import { envVars } from '../config/env';
+import { IAuthProvider, Role } from '../modules/user/user.interface';
+import { User } from '../modules/user/user.model';
+
+export const sendAdmin = async () => {
+    try {
+        const isSuperAdminExist = await User.findOne({
+            email: envVars.ADMIN_EMAIL,
+        });
+
+        if (isSuperAdminExist) {
+            console.log('Admin Already Exists!!');
+            return;
+        }
+
+        const hashedPassword = await bcryptjs.hash(
+            envVars.ADMIN_PASSWORD as string,
+            Number(envVars.BCRYPT_SALT_ROUNDS)
+        );
+
+        const authProvider: IAuthProvider = {
+            provider: 'credential',
+            providerId: envVars.ADMIN_EMAIL,
+        };
+
+        const payload = {
+            name: 'Admin',
+            role: Role.ADMIN,
+            email: envVars.ADMIN_EMAIL,
+            password: hashedPassword,
+            isValid: true,
+            auths: [authProvider],
+        };
+
+        const Admin = await User.create(payload);
+        console.log(Admin);
+    } catch (error) {
+        console.log(error);
+    }
+};
